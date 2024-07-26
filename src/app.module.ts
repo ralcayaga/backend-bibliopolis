44c719d5e1d37e4c6b6b86047233275279963102
ralcayag <ralcayaga74@gmail.com>
2024-07-25 21:34:03 -0400

    Módulo de usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { LibrosModule } from './libros/libros.module';
 
 @Module({
-  imports: [UsuariosModule],
+  imports: [UsuariosModule, LibrosModule],
   controllers: [AppController],
   providers: [AppService],
 })
